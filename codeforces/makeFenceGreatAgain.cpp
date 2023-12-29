42a3c58c2c97c8c2688ffdfa268d4fae39c28aad
Rohitaas Beri <rohitaas.beri@servicenow.com>
2023-12-29 23:21:05 +0530

    3.5/5

--- codeforces/makeFenceGreatAgain.cpp
+++ codeforces/makeFenceGreatAgain.cpp
@@ -1,45 +1,51 @@
-/*There are 𝑛
- people on the number line; the 𝑖
--th person is at point 𝑎𝑖
- and wants to go to point 𝑏𝑖
-. For each person, 𝑎𝑖<𝑏𝑖
-, and the starting and ending points of all people are distinct. (That is, all of the 2𝑛
- numbers 𝑎1,𝑎2,…,𝑎𝑛,𝑏1,𝑏2,…,𝑏𝑛
- are distinct.)
+/*outputstandard output
+You have a fence consisting of 𝑛
+ vertical boards. The width of each board is 1
+. The height of the 𝑖
+-th board is 𝑎𝑖
+. You think that the fence is great if there is no pair of adjacent boards having the same height. More formally, the fence is great if and only if for all indices from 2
+ to 𝑛
+, the condition 𝑎𝑖−1≠𝑎𝑖
+ holds.
 
-All the people will start moving simultaneously at a speed of 1
- unit per second until they reach their final point 𝑏𝑖
-. When two people meet at the same point, they will greet each other once. How many greetings will there be?
+Unfortunately, it is possible that now your fence is not great. But you can change it! You can increase the length of the 𝑖
+-th board by 1
+, but you have to pay 𝑏𝑖
+ rubles for it. The length of each board can be increased any number of times (possibly, zero).
 
-Note that a person can still greet other people even if they have reached their final point.
+Calculate the minimum number of rubles you have to spend to make the fence great again!
+
+You have to answer 𝑞
+ independent queries.
 
 Input
-The first line of the input contains a single integer 𝑡
- (1≤𝑡≤104
-) — the number of test cases. The description of test cases follows.
+The first line contains one integer 𝑞
+ (1≤𝑞≤3⋅105
+) — the number of queries.
 
-The first line of each test case contains a single integer 𝑛
- (1≤𝑛≤2⋅105
-) — the number of people.
+The first line of each query contains one integers 𝑛
+ (1≤𝑛≤3⋅105
+) — the number of boards in the fence.
 
-Then 𝑛
- lines follow, the 𝑖
--th of which contains two integers 𝑎𝑖
+The following 𝑛
+ lines of each query contain the descriptions of the boards. The 𝑖
+-th line contains two integers 𝑎𝑖
  and 𝑏𝑖
- (−109≤𝑎𝑖<𝑏𝑖≤109
-) — the starting and ending positions of each person.
+ (1≤𝑎𝑖,𝑏𝑖≤109
+) — the length of the 𝑖
+-th board and the price for increasing it by 1
+, respectively.
 
-For each test case, all of the 2𝑛
- numbers 𝑎1,𝑎2,…,𝑎𝑛,𝑏1,𝑏2,…,𝑏𝑛
- are distinct.
+It is guaranteed that sum of all 𝑛
+ over all queries not exceed 3⋅105
+.
 
-The sum of 𝑛
- over all test cases does not exceed 2⋅105
+It is guaranteed that answer to each query will not exceed 1018
 .
 
 Output
-For each test case, output a single integer denoting the number of greetings that will happen.*/
-//https://codeforces.com/contest/1915/problem/F
+For each query print one integer — the minimum number of rubles you have to spend to make the fence great.
+https://codeforces.com/contest/1221/problem/D*/
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -123,59 +129,33 @@ void fill(Primitive Value, Complex &C, ComplexObjects &...list)
 }
 //-------------------------------------------------------------------------------
 
-const int N = 2e5+2;
-int bit[N];
-void update(int index , int val)
-{
-    while(index<N)
-    {
-        bit[index]+=val;
-        index+=index&(-index);
-    }
-}
-
-int query(int index )
-{
-    int ans =0;
-    while(index>0)
-    {
-        ans+=bit[index];
-        index-=index&(-index);
-    }
-    return ans;
-}
-
 void solve()
 {
 
-    memset(bit,0,sizeof(bit));
-    vector<pair<int,int> > co;
-    int n;cin>>n;
-    map<int,int> compress;
-
-    for(int i =0;i<n;i++)
+    int n;
+    cin >> n;
+    int a[n + 1], b[n + 1], dp[n + 1][3];
+    memset(a, 0, sizeof(a)), memset(b, 0, sizeof(b)), memset(dp, 0, sizeof(dp));
+    for (int i = 0; i < n; i++)
     {
-        int a,b;cin>>a>>b;
-        co.push_back({a,b});
-        compress[b]++;
+        cin >> a[i + 1] >> b[i + 1];
     }
 
-    int cnt =1;
-    for(auto &i : compress)
+    for (int i = 1; i <= n; i++)
     {
-        i.second=cnt++;
+        for (int j = 0; j <= 2; j++)
+        {
+            dp[i][j] = INF;
+            for (int k = 0; k <= 2; k++)
+            {
+                if (a[i] - a[i - 1] != k - j)
+                {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][k] + j * b[i]);
+                }
+            }
+        }
     }
-
-    sort(all(co));
-    int ans =0,total=0;
-    for(auto i : co)
-    {
-        ans += total - query(compress[i.second]);
-        total++;
-        update(compress[i.second],1);
-    }
-    print(ans);
-  
+    print(min({dp[n][0], dp[n][1], dp[n][2]}));
 }
 
 signed main()
