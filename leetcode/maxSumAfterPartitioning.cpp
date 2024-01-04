d4361e6a8b82fe440246db26e1e6bbc7c2b0b81a
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-01-05 01:22:21 +0530

    3/5

--- leetcode/maxSumAfterPartitioning.cpp
+++ leetcode/maxSumAfterPartitioning.cpp
@@ -1,3 +1,7 @@
+/*Given an integer array arr, partition the array into (contiguous) subarrays of length at most k. After partitioning, each subarray has their values changed to become the maximum value of that subarray.
+
+Return the largest sum of the given array after partitioning. Test cases are generated so that the answer fits in a 32-bit integer.
+https://leetcode.com/problems/partition-array-for-maximum-sum/description/*/
 #include <bits/stdc++.h>
 using namespace std;
 class Solution
@@ -7,28 +11,16 @@ public:
     {
         int n = arr.size();
         arr.insert(arr.begin(), 0);
-
-        int Max[n + 1][n + 1];
-        memset(Max, 0, sizeof(Max));
-        for (int i = 1; i <= n; i++)
-            Max[i][i] = arr[i];
-
-        for (int len = 2; len <= k; len++)
-        {
-            for (int i = 1; i + len - 1 <= n; i++)
-            {
-                Max[i][i + len - 1] = max(Max[i + 1][i + len - 1], arr[i]);
-            }
-        }
-
         int dp[n + 1];
         memset(dp, 0, sizeof(dp));
         dp[1] = arr[1];
         for (int i = 2; i <= n; i++)
         {
+            int mx = arr[i];
             for (int j = i - 1; i - j <= k && j >= 0; j--)
             {
-                dp[i] = max(dp[i], dp[j] + (i - j) * Max[j + 1][i]);
+                dp[i] = max(dp[i], dp[j] + (i - j) * mx);
+                mx = max(mx, arr[j]);
             }
         }
         return dp[n];
