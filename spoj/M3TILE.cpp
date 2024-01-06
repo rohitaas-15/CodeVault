910a886d9b2ebab34cb058de026b494c057b22d1
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-01-06 22:57:07 +0530

    3.5/5

---
+++ spoj/M3TILE.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+long long int _debug = 0, _nested = 0, _t = 1, INF = 1e18 + 2;
+// macros-------------------------------------------------------------------------------
+#define ll long long
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rep(i, x, n) for (int(i) = (x); (i) < (n); (i)++)
+#define endl "\n"
+#define nl        \
+    cout << endl; \
+    if (_debug)   \
+        cerr << endl;
+
+// print
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void print(Primitive &&P)
+{
+    cout << P << " ";
+    if (_debug)
+        cerr << P << " ";
+    if (!_nested)
+        nl;
+}
+template <typename Complex>
+void print(vector<Complex> &Vector)
+{
+    _nested = 1;
+    for (Complex &C : Vector)
+        print(C);
+    nl;
+    _nested = 0;
+}
+template <typename Complex, typename... ComplexObjects>
+void print(Complex &&C, ComplexObjects &&...list)
+{
+    _nested = 1, print(C);
+    _nested = 0, print(list...);
+}
+
+// scan
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void scan(Primitive &P)
+{
+    cin >> P;
+}
+template <typename Complex>
+void scan(vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        scan(C);
+}
+template <typename Complex, typename... ComplexObjects>
+void scan(Complex &C, ComplexObjects &...list)
+{
+    scan(C), scan(list...);
+}
+
+// fill bulk
+// data--------------------------------------------------------------------------------
+template <typename Primitive>
+void fill(Primitive Value, Primitive &P)
+{
+    P = Value;
+}
+template <typename Primitive, typename Complex>
+void fill(Primitive Value, vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        fill(Value, C);
+}
+template <typename Primitive, typename Complex, typename... ComplexObjects>
+void fill(Primitive Value, Complex &C, ComplexObjects &...list)
+{
+    fill(Value, C), fill(Value, list...);
+}
+
+// policy based data
+// structures---------------------------------------------------------------------------
+template <typename T>
+using indexed_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T1, typename T2>
+using indexed_map =
+    tree<T1, T2, less<T1>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// code----------------------------------------------------------------------------------
+
+const int N = 31;
+void solve()
+{
+    int f[N], g[N]; // f(n)= number of ways to fill a 3*n grid, g(n)=no. of ways to fill a 3*n grid with 1 extra square at the bottom row
+                    // f(n)= f(n-2)+2*g(n-1), g(n)=f(n-1)+g(n-2)
+    f[0] = 1;
+    f[1] = 0, f[2] = 3;
+    g[1] = 1, g[2] = 0;
+    for (int i = 3; i < N; i++)
+    {
+        f[i] = f[i - 2] + 2 * g[i - 1];
+        g[i] = f[i - 1] + g[i - 2];
+    }
+
+    while (true)
+    {
+        int n;
+        cin >> n;
+        if (n == -1)
+            return;
+        print(f[n]);
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    // cin >> _t;
+    for (int tc = 1; tc <= _t; tc++)
+        solve();
+}
+
+// cin does not move the input cursor to the next line, use cin.ignore() to
+// move it to the next line use an array instead of an int whenever possible,
+// for coding competitions
