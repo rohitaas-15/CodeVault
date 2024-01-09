c1022b2e14e75f687ecc59923abb1eabdc69069e
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-01-09 23:58:10 +0530

    3.75/5

--- codeforces/kyoyaAndColoredBalls.cpp
+++ codeforces/kyoyaAndColoredBalls.cpp
@@ -1,3 +1,15 @@
+/*Kyoya Ootori has a bag with n colored balls that are colored with k different colors. The colors are labeled from 1 to k. Balls of the same color are indistinguishable. He draws balls from the bag one by one until the bag is empty. He noticed that he drew the last ball of color i before drawing the last ball of color i + 1 for all i from 1 to k - 1. Now he wonders how many different ways this can happen.
+
+Input
+The first line of input will have one integer k (1 ≤ k ≤ 1000) the number of colors.
+
+Then, k lines will follow. The i-th line will contain ci, the number of balls of the i-th color (1 ≤ ci ≤ 1000).
+
+The total number of balls doesn't exceed 1000.
+
+Output
+A single integer, the number of ways that Kyoya can draw the balls from the bag as described in the statement, modulo 1 000 000 007
+https://codeforces.com/problemset/problem/553/A*/
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
