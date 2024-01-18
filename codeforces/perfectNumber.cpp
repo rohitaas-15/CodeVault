98cffaba6bc4badbe0a8ac17a7abfe32b9299b46
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-01-18 17:09:49 +0530

    3/5

---
+++ codeforces/perfectNumber.cpp
@@ -0,0 +1,157 @@
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+long long int _debug = 0, _nested = 0, _t = 1, INF = 1e18 + 2;
+// macros-------------------------------------------------------------------------------
+#define ll long long
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rep(i, x, n) for (int(i) = (x); (i) < (n); (i)++)
+#define endl "\n"
+#define nl        \
+    cout << endl; \
+    if (_debug)   \
+        cerr << endl;
+
+// print
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void print(Primitive &&P)
+{
+    cout << P << " ";
+    if (_debug)
+        cerr << P << " ";
+    if (!_nested)
+    {
+        nl;
+    }
+}
+template <typename Complex>
+void print(vector<Complex> &Vector)
+{
+    _nested = 1;
+    for (Complex &C : Vector)
+        print(C);
+    nl;
+    _nested = 0;
+}
+template <typename Complex, typename... ComplexObjects>
+void print(Complex &&C, ComplexObjects &&...list)
+{
+    _nested = 1, print(C);
+    _nested = 0, print(list...);
+}
+
+// scan
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void scan(Primitive &P)
+{
+    cin >> P;
+}
+template <typename Complex>
+void scan(vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        scan(C);
+}
+template <typename Complex, typename... ComplexObjects>
+void scan(Complex &C, ComplexObjects &...list)
+{
+    scan(C), scan(list...);
+}
+
+// fill bulk
+// data--------------------------------------------------------------------------------
+template <typename Primitive>
+void fill(Primitive Value, Primitive &P)
+{
+    P = Value;
+}
+template <typename Primitive, typename Complex>
+void fill(Primitive Value, vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        fill(Value, C);
+}
+template <typename Primitive, typename Complex, typename... ComplexObjects>
+void fill(Primitive Value, Complex &C, ComplexObjects &...list)
+{
+    fill(Value, C), fill(Value, list...);
+}
+
+// policy based data
+// structures---------------------------------------------------------------------------
+template <typename T>
+using indexed_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T1, typename T2>
+using indexed_map =
+    tree<T1, T2, less<T1>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// code----------------------------------------------------------------------------------
+int k;
+void perfect(string no, int digits, int &cnt, int rem)
+{
+    if (!rem && !digits)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            print(no);
+            exit(0);
+        }
+        return;
+    }
+
+
+    if (digits == 1)
+    {
+        if (rem <= 9 && rem >= 0)
+        {
+            no.push_back(rem + '0');
+            perfect(no, digits - 1, cnt, rem - rem);
+            no.pop_back();
+        }
+        return;
+    }
+    for (int i = 0; i <= 9; i++)
+    {
+        no.push_back(i + '0');
+        perfect(no, digits - 1, cnt, rem - i);
+        no.pop_back();
+    }
+}
+
+void solve()
+{
+    cin >> k;
+    int cnt = 0;
+    for (int digits = 1; digits <= 1000; digits++)
+    {
+        for (int first = 1; first <= 9; first++)
+        {
+            perfect(to_string(first), digits, cnt, 10-first);
+        }
+    }
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    // cin >> _t;
+    for (int tc = 1; tc <= _t; tc++)
+    {
+        // cout << "Case " << tc << ":"
+        //      << " ";
+        solve();
+    }
+}
+
+// cin does not move the input cursor to the next line, use cin.ignore() to
+// move it to the next line use an array instead of an int whenever possible,
+// for coding competitions
