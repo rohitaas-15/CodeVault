7d41dc10e61c95a2171dfde77730ce8c2537a79d
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-01-20 00:51:05 +0530

    3.75/5

---
+++ spoj/PT07X.cpp
@@ -0,0 +1,154 @@
+/*You are given an unweighted, undirected tree. Write a program to find a vertex set of minimum size in this tree such that each edge has as least one of its end-points in that set.
+
+Input
+The first line of the input file contains one integer N --- number of nodes in the tree (0 < N <= 100000). Next N-1 lines contain N-1 edges of that tree --- Each line contains a pair (u, v) means there is an edge between node u and node v (1 <= u,v <= N).
+
+Output
+Print number of nodes in the satisfied vertex set on one line.
+https://www.spoj.com/problems/PT07X/*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+long long int _debug = 0, _nested = 0, _t = 1, INF = 1e18 + 2;
+// macros-------------------------------------------------------------------------------
+#define ll long long
+#define int ll
+#define all(x) x.begin(), x.end()
+#define rep(i, x, n) for (int(i) = (x); (i) < (n); (i)++)
+#define endl "\n"
+#define nl        \
+    cout << endl; \
+    if (_debug)   \
+        cerr << endl;
+
+// print
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void print(Primitive &&P)
+{
+    cout << P << " ";
+    if (_debug)
+        cerr << P << " ";
+    if (!_nested)
+    {
+        nl;
+    }
+}
+template <typename Complex>
+void print(vector<Complex> &Vector)
+{
+    _nested = 1;
+    for (Complex &C : Vector)
+        print(C);
+    nl;
+    _nested = 0;
+}
+template <typename Complex, typename... ComplexObjects>
+void print(Complex &&C, ComplexObjects &&...list)
+{
+    _nested = 1, print(C);
+    _nested = 0, print(list...);
+}
+
+// scan
+// helper-------------------------------------------------------------------------------
+template <typename Primitive>
+void scan(Primitive &P)
+{
+    cin >> P;
+}
+template <typename Complex>
+void scan(vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        scan(C);
+}
+template <typename Complex, typename... ComplexObjects>
+void scan(Complex &C, ComplexObjects &...list)
+{
+    scan(C), scan(list...);
+}
+
+// fill bulk
+// data--------------------------------------------------------------------------------
+template <typename Primitive>
+void fill(Primitive Value, Primitive &P)
+{
+    P = Value;
+}
+template <typename Primitive, typename Complex>
+void fill(Primitive Value, vector<Complex> &Vector)
+{
+    for (Complex &C : Vector)
+        fill(Value, C);
+}
+template <typename Primitive, typename Complex, typename... ComplexObjects>
+void fill(Primitive Value, Complex &C, ComplexObjects &...list)
+{
+    fill(Value, C), fill(Value, list...);
+}
+
+// policy based data
+// structures---------------------------------------------------------------------------
+template <typename T>
+using indexed_set =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <typename T1, typename T2>
+using indexed_map =
+    tree<T1, T2, less<T1>, rb_tree_tag, tree_order_statistics_node_update>;
+
+// code----------------------------------------------------------------------------------
+vector<vector<int>> adj;
+const int N = 1e5 + 2;
+int dp[N][2];
+
+void dfs(int node, int par)
+{
+    int take = 0, leave = 0;
+    for (auto i : adj[node])
+    {
+        if (i != par)
+        {
+            dfs(i, node);
+            take += min(dp[i][0], dp[i][1]);
+            leave += dp[i][1];
+        }
+    }
+    dp[node][0] = leave, dp[node][1] = take + 1;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    adj.clear(), adj.resize(n + 1);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
+    }
+    memset(dp, 0ll, sizeof(dp));
+    dfs(1, 0);
+    print(min(dp[1][0], dp[1][1]));
+}
+
+signed main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    // cin >> _t;
+    for (int tc = 1; tc <= _t; tc++)
+    {
+        // cout << "Case " << tc << ":"
+        //      << " ";
+        solve();
+    }
+}
+
+// cin does not move the input cursor to the next line, use cin.ignore() to
+// move it to the next line use an array instead of an int whenever possible,
+// for coding competitions
