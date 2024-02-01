c39515764aea89a159d3e647b40d8a8cf885940d
rohitaas.beri <rohitaas.beri@servicenow.com>
2024-02-01 23:46:28 +0530

    3/5

---
+++ leetcode/longestCommonSubstring.cpp
@@ -0,0 +1,52 @@
+
+/*https://www.geeksforgeeks.org/problems/longest-common-substring1452/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab*/
+//Given two strings. The task is to find the length of the longest common substring.
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+public:
+    int longestCommonSubstr(string S1, string S2, int n, int m)
+    {
+        int dp[n + 1][m + 1];
+        memset(dp, 0, sizeof(dp)); //longest common substring when the answer for the first substring ends at ith index, and substr for second string ends at jth index
+
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (S1[i - 1] == S2[j - 1])
+                {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = max(ans, dp[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr(s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
